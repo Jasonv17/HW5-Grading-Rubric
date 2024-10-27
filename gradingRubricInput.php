c2a31ffeee2f4c73102d1b4f60f5a6a104858581
Jason Valerio-Peralta <jasonvalerioperalta17@gmail.com>
2024-10-27 17:29:30 -0400

    Updated gradingRubricInput.php

--- gradingRubricInput.php
+++ gradingRubricInput.php
@@ -1,8 +1,8 @@
 
 <?php
 $servername = "localhost";
-$username = "csc350";
-$password = "xampp";
+$username = "root";
+$password = "";
 $dbname = "school_grades";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
